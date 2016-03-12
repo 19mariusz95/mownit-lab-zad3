cfff8fad3ce7ac0e2bc5128302236ef901692856
Mariusz <19mariusz95@gmail.com>
2016-03-12 12:15:20 +0100

    edge class

---
+++ src/main/java/graph/Edge.java
@@ -0,0 +1,20 @@
+package graph;
+
+/**
+ * Created by Mariusz on 12.03.2016.
+ */
+public class Edge {
+    private double resistance;
+
+    public Edge(double resistance) {
+        this.resistance = resistance;
+    }
+
+    public double getResistance() {
+        return resistance;
+    }
+
+    public void setResistance(double resistance) {
+        this.resistance = resistance;
+    }
+}
