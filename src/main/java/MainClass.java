bb1b3641dc5ebb5c9361b729a76cbf48385178cd
Mariusz <19mariusz95@gmail.com>
2016-03-12 14:01:06 +0100

    MainClass

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,50 @@
+import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.algorithms.layout.Layout;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.visualization.BasicVisualizationServer;
+import graph.Edge;
+import graph.Vertex;
+import parser.InputFileParser;
+import solver.VoltageSolver;
+import voltage.Voltage;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.logging.Logger;
+
+/**
+ * Created by Mariusz on 12.03.2016.
+ */
+public class MainClass {
+    public static Logger logger = Logger.getLogger(MainClass.class.getName());
+
+    public static void main(String[] args) {
+        InputFileParser<Vertex, Edge> inputFileParser = null;
+        try {
+            File file = new File(args[0]);
+            inputFileParser = new InputFileParser<>(Vertex::new, Edge::new, file);
+            inputFileParser.parseInput();
+        } catch (ArrayIndexOutOfBoundsException | FileNotFoundException e) {
+            logger.warning(e.getMessage());
+            System.exit(1);
+        }
+        Graph<Vertex, Edge> graph = inputFileParser.getGraph();
+        Voltage<Vertex> voltage = inputFileParser.getVoltage();
+        VoltageSolver<Vertex, Edge> voltageSolver = new VoltageSolver<>(graph, voltage);
+        Map<Edge, Double> voltages = voltageSolver.solve();
+        Layout<Vertex, Edge> circleLayout = new CircleLayout<>(graph);
+        BasicVisualizationServer<Vertex, Edge> basicVisualizationServer = new BasicVisualizationServer<>(circleLayout);
+        basicVisualizationServer.getRenderContext().setEdgeLabelTransformer(edge -> voltages.get(edge).toString());
+        basicVisualizationServer.setPreferredSize(new Dimension(800, 800));
+
+        JFrame frame = new JFrame("graph");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(basicVisualizationServer);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
