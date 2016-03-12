6c72ca737f9c090599f34e26739ebf2e2141d7f1
Mariusz <19mariusz95@gmail.com>
2016-03-12 13:57:42 +0100

    edge voltage attribute deleted

--- src/main/java/graph/Edge.java
+++ src/main/java/graph/Edge.java
@@ -5,19 +5,9 @@ package graph;
  */
 public class Edge {
     private double resistance;
-    private double voltage;
 
     public Edge(double resistance) {
         this.resistance = resistance;
-        this.voltage = 0.0;
-    }
-
-    public double getVoltage() {
-        return voltage;
-    }
-
-    public void setVoltage(double voltage) {
-        this.voltage = voltage;
     }
 
     public double getResistance() {
