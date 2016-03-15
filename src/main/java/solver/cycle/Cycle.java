9a7877acacc8204c843f235624d78379da58e73e
Mariusz <19mariusz95@gmail.com>
2016-03-15 12:56:22 +0100

    generic cycle class

--- src/main/java/solver/cycle/Cycle.java
+++ src/main/java/solver/cycle/Cycle.java
@@ -1,29 +1,27 @@
 package solver.cycle;
 
-import graph.Edge;
-
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Mariusz on 15.03.2016.
  */
-public class Cycle {
-    private List<Edge> edgeList;
+public class Cycle<E> {
+    private List<E> edgeList;
 
     public Cycle() {
         this.edgeList = new ArrayList<>();
     }
 
-    public Cycle(List<Edge> edgeList) {
+    public Cycle(List<E> edgeList) {
         this.edgeList = edgeList;
     }
 
-    public void addEdge(Edge e) {
+    public void addEdge(E e) {
         this.edgeList.add(e);
     }
 
-    public List<Edge> getEdgeList() {
+    public List<E> getEdgeList() {
         return edgeList;
     }
 }
